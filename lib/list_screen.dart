3ea891e1c76bdf27ae5cdcd57896490286eee88e
ParkGiTeak <rl980901@naver.com>
2025-07-14 12:09:40 +0900

    feat: ListScreen의 각 Todo Item에 대한 onTap Action, onDelete Action을 추가

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/create_screen.dart';
+import 'package:todo_list/todo_item.dart';
 
 import 'main.dart';
-import 'model/todo.dart';
 
 class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
@@ -19,14 +19,29 @@ class _ListScreenState extends State<ListScreen> {
         title: Text('Todo 리스트'),
       ),
       body: ListView(
-        children: todos.values
-            .map(
-              (todo) => ListTile(
-                title: Text(todo.title),
-                subtitle: Text('${todo.dateTime}'),
-              ),
-            )
-            .toList(),
+        children: todos.values.map((e) {
+          return TodoItem(
+            todo: e,
+            onTap: (todo) async {
+              todo.isDone = !todo.isDone;
+              if (todos.get(todo.id) != null) {
+                await todos.put(todo.id, todo);
+              } else {
+                throw ('currentTodo is not contains DB');
+              }
+
+              setState(() {});
+            },
+            onDelete: (todo) async {
+              if (todos.get(todo.id) != null) {
+                await todos.delete(todo.id);
+              } else {
+                throw ('currentTodo is not contains DB');
+              }
+              setState(() {});
+            },
+          );
+        }).toList(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
