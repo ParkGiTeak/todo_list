7669b5957d3b1df1897c42193a21336fb33ffbc2
ParkGiTeak <rl980901@naver.com>
2025-07-14 12:05:15 +0900

    feat: Todo Model Class isDone field 추가, id 값 UUID로 받기 위해서 String Type으로 바꿈

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -5,14 +5,18 @@ part 'todo.g.dart';
 @HiveType(typeId: 0)
 class Todo {
   @HiveField(0)
-  int? id;
+  String id;
   @HiveField(1)
   String title;
   @HiveField(2)
   int dateTime;
+  @HiveField(3)
+  bool isDone;
 
   Todo({
+    required this.id,
     required this.title,
     required this.dateTime,
+    this.isDone = false,
   });
 }
